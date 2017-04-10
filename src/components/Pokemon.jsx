ed78e3c75f8a1af82f7848e0334a2e555357a9a4
Kat Robbie <katherine.robbie@gmail.com>
2017-04-11 11:52:19 +1200

    started pokemon api

---
+++ src/components/Pokemon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {fetchPokemon} from '../actions/PokeAPI'
+
+const Pokemon = () => (
+  <div>
+    Bulbasaur
+  </div>
+)
+
+export default Pokemon
